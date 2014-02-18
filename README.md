816ee20a5244a166cc25611e0bf8ccba18a8e584
Jefferson Lam <jlam520@gmail.com>
2014-02-17 17:58:01 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Daytripper
+=================
+By: Jefferson Lam
+Last Updated: January 24nd, 2014
+
+Special Thanks To:
+    Google
+    CodingDojo : http://codingdojo.com/
+
+    For the inspiration:
+    Brian Folts : http://www.brianfolts.com/driver/
+    The Folks behind HyperLapse : http://hyperlapse.tllabs.io/
+
+    For the loading spinner:
+    Felix Gnass : fgnass.github.com
+
+
+If you're reading this, you're looking at the first public iteration of this project. My code hasn't quite been optimized yet, but it's on my to-do list. Also, I'm open to any more suggestions for a project name, cause I've been changing the name of this thing pretty often. You will have my utmost thanks and an internet high-five.
+
+Thanks!
+
+-Jefferson Lam
+February 17, 2014
---
+++ streetviewpoint.js
@@ -0,0 +1,31 @@
+//streetviewpoint.js
+
+//Class: StreetViewPoint
+//========================
+//Generates and stores information of a single street view image call,
+//generating the source URL and latLng.
+
+function StreetViewPoint(latLng, heading, imgOptions){
+	var imgOptions = imgOptions;
+	this.latLng = latLng;
+	this.heading = heading;
+	var self = this;
+
+	function createSrc(){
+        var src = "http://maps.googleapis.com/maps/api/streetview?location="+
+                    self.latLng.toUrlValue()+
+                    "&heading="+
+                    self.heading+
+                    "&size="+
+                    imgOptions.width+"x"+imgOptions.height+
+                    "&fov="+
+                    imgOptions.fov+
+                    "&pitch="+
+                    imgOptions.pitch+
+                    "&sensor=false&key="+
+                    imgOptions.key;
+        self.src = src;
+    }
+
+    createSrc();
+}
